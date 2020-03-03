9ed365d3e685432a45252c61f950c5e270478654
Zaid Quadri <zquadri98@gmail.com>
2020-03-03 17:57:59 -0500

    adding database and shrink route

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-from flask import Flask, render_template
-import random 
-import string 
+from flask import Flask, render_template, request, redirect
+import random
+import string
 import sqlite3
-
-app = Flask(__name__) 
-
-@app.route('/') 
-def main(): 
-    return render_template('index.html')
-
-@app.route('/shrink', methods=['POST'])
-def shrink():
-    #https://google.com/340i930f9s0dfsf
-    #https://linker.dev/
-    #generate short id
-    #map it to real url in db
-    new_url = "lynker.dev/" + generate_random_id(3)
-    return new_url
-  
-
-
-
-
-def generate_random_id(id_length):
-    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=id_length))
-    return id
-
+from flask import jsonify
+from urllib.parse import urlparse
 
 # def create_connection(db_file):
 #     """ create a database connection to the SQLite database
@@ -38,9 +16,66 @@ def generate_random_id(id_length):
 #         conn = sqlite3.connect(db_file)
 #     except Error as e:
 #         print(e)
- 
+
 #     return conn
 
 
-if __name__ == '__main__': 
-    app.run(port=3001,debug=True)
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/shrink', methods=['POST'])
+def shrink():
+    # connect to db
+    conn = sqlite3.connect('lynker.db')
+    c = conn.cursor()
+    body = request.json
+    url = body['url']
+    c.execute('SELECT link_id FROM links WHERE original_link = ?', [url])
+    results = c.fetchone()
+    l_id = ''
+    if(results == None):
+        l_id = generate_random_id(3)
+        print(l_id, url)
+        c.execute('''
+        INSERT INTO links (link_id, original_link) VALUES (?,?)
+        ''', (l_id, url))
+        conn.commit()
+
+    else:
+        l_id = results[0]
+
+    return "lynker.dev/" + l_id
+
+
+@app.route('/<l_id>')
+def link_redirect(l_id):
+    print(l_id)
+    conn = sqlite3.connect('lynker.db')
+    c = conn.cursor()
+    c.execute('SELECT original_link FROM links WHERE link_id = ?', [l_id])
+    results = c.fetchone()
+    print(results)
+    url = results[0]
+    print(url)
+    if(url.startswith("http://") or url.startswith("https://")):
+        return redirect(url)
+    return redirect("http://" + url)
+
+
+# @app.route('/expand', methods=['GET'])
+# def expand():
+
+
+def generate_random_id(id_length):
+    id = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=id_length))
+    return id
+
+
+if __name__ == '__main__':
+    app.run(port=3001, debug=True)
--- static/js/lynker.js
+++ static/js/lynker.js
@@ -1,9 +1,26 @@
-const btn = document.querySelector('#shrink-btn');
-const succsessBox = document.querySelector('#success-box');
+const shrinkBtn = document.querySelector('#shrink-btn');
+const successBox = document.querySelector('#success-box');
 const errorBox = document.querySelector('#error-box');
 const main = document.querySelector('main');
-btn.addEventListener('click', function() {
-  fetch('/shrink', { method: 'POST' })
+const urlInput = document.querySelector('#url-input');
+
+//hide buttons and show loader
+shrinkBtn.addEventListener('click', function() {
+  shrinkBtn.classList.add('hide');
+
+  const reqBody = {
+    url: urlInput.value
+  };
+
+  const fetchConfig = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  };
+
+  fetch('/shrink', fetchConfig)
     .then(handleResponse)
     .then(handleText)
     .catch(handleError);
@@ -18,7 +35,7 @@ function handleText(text) {
     document.getElementById('success-box').remove();
   }
   let box = document.createElement('div');
-  box.textContent = text;
+  box.innerHTML = text;
   box.setAttribute('id', 'success-box');
   main.appendChild(box);
 }
